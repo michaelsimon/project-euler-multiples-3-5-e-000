f3a5f262dbedb987791cfcd6327a721edfbac1e3
michaelsimon <michael.simon@gmail.com>
2015-12-22 22:32:40 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+def collect_multiples(value)
+  counter = 1
+  multiples =Array.new
+  until counter == value
+    if ((counter % 3 == 0) and (counter % 5 == 0)) || (counter % 3 == 0) || (counter % 5 == 0)
+      multiples << counter
+    end
+    counter += 1
+  end
+  return multiples
+end 
+
+def sum_multiples(value)
+
+  counter = 1
+  multiples =Array.new
+  sum = 0
+  until counter == value
+    if ((counter % 3 == 0) and (counter % 5 == 0)) || (counter % 3 == 0) || (counter % 5 == 0)
+      multiples << counter
+    end
+    counter += 1
+  end
+  multiples.each do |multiple|
+    sum += multiple
+  end
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,38 @@
 # Enter your object-oriented solution here!
+class Multiples
+attr_accessor :limit
+
+def initialize(limit)
+  @limit = limit
+end
+
+def collect_multiples
+  counter = 1
+  multiples = Array.new
+  until counter == @limit
+    if ((counter % 3 == 0) and (counter % 5 == 0)) || (counter % 3 == 0) || (counter % 5 == 0)
+      multiples << counter
+    end
+    counter += 1
+  end
+  return multiples
+end 
+
+def sum_multiples
+  counter = 1
+  multiples =Array.new
+  sum = 0
+  until counter == @limit
+    if ((counter % 3 == 0) and (counter % 5 == 0)) || (counter % 3 == 0) || (counter % 5 == 0)
+      multiples << counter
+    end
+    counter += 1
+  end
+  multiples.each do |multiple|
+    sum += multiple
+  end
+  return sum
+end
+
+
+end
